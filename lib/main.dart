5cba3a482aa630dc740d2f8b4afbffdb24365509
Keano <ddff1456@gmail.com>
2021-12-21 00:31:45 +0900

    firrst commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar : AppBar(title : Text('앱임')),
+
+
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: double.infinity, height: 70, color: Colors.blue,
+          ),
+        ),
+
+
+
+
+      )
+
+    );
+  }
+}
